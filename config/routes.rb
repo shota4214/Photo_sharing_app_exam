12f5906f22b8c6b4af8b4d7ce6b2350d9a5f6a8e
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-20 22:59:26 +0900

    routesにroot users#new追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  root 'users#new'
   resources :favorites, only:[:create, :destroy, :index]
   resources :sessions, only:[:new, :create, :destroy]
   resources :users, only:[:new, :create, :show, :update, :edit] do
