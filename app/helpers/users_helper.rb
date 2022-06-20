1495644a62d984f6d3525e4a2b91d90afa14e515
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-20 22:27:05 +0900

    style:不要な行やインデント削除・不要ファイル削除

--- app/helpers/users_helper.rb
+++
@@ -1,9 +0,0 @@
-module UsersHelper
-def user_choose_new_or_edit
-  if action_name == 'new' || action_name == 'create'
-    confirm_users_path
-    elsif action_name == 'edit'
-      user_path
-    end
-  end
-end
