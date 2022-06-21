5fd857b6bc8de57b41cfdb622b00fcd959075e66
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-21 13:47:03 +0900

    fix:herokuデプロイ500エラーmigrateやり直し

---
+++ db/migrate/20220616041549_create_pictures.rb
@@ -0,0 +1,10 @@
+class CreatePictures < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pictures do |t|
+    t.text "image"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    end
+  end
+end
